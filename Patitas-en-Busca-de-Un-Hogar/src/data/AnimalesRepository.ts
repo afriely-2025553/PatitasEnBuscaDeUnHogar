c4143ca9e942a0f84e74753f3f7cb16ba4d56b86
afrily-2025553 <afriely-2025553@kinal.edu.gt>
2026-07-26 03:25:41 -0600

    feat: se creo el AnimalesRespository con el CRUD funcional necesario

---
+++ Patitas-en-Busca-de-Un-Hogar/src/data/AnimalesRepository.ts
@@ -0,0 +1,43 @@
+import { readFile, writeFile } from "fs/promises";
+import { Animal } from "../models/Animal";
+
+export class AnimalesRepository {
+
+    private ruta = "./src/data/animales.json";
+
+    async obtenerAnimales(): Promise<Animal[]> {
+
+        try {
+
+            const datos = await readFile(this.ruta, "utf-8");
+
+            return JSON.parse(datos);
+
+        } catch (error) {
+
+            console.log("Error al leer el archivo de animales.");
+
+            return [];
+
+        }
+
+    }
+
+    async guardarAnimales(animales: Animal[]): Promise<void> {
+
+        try {
+
+            await writeFile(
+                this.ruta,
+                JSON.stringify(animales, null, 4)
+            );
+
+        } catch (error) {
+
+            console.log("Error al guardar el archivo de animales.");
+
+        }
+
+    }
+
+}
