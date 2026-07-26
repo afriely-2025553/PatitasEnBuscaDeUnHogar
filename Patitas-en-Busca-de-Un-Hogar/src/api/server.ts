f6a18e4da27b679226fc01f89995a70f5c251608
afrily-2025553 <afriely-2025553@kinal.edu.gt>
2026-07-26 03:23:15 -0600

    feat: se creo el archivo server y se vreo la ruta del servidor

---
+++ Patitas-en-Busca-de-Un-Hogar/src/api/server.ts
@@ -0,0 +1,17 @@
+import { createServer } from "http";
+import { routes } from "./animalRouter";
+
+const servidor = createServer(async (req, res) => {
+
+    await routes(req, res);
+
+});
+
+servidor.listen(3000, () => {
+
+    console.log("===================================");
+    console.log("Servidor iniciado");
+    console.log("http://localhost:3000");
+    console.log("===================================");
+
+});
