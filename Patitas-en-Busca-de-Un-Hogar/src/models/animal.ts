061225df316304041c94f984643ce4c1ccefd12e
afrily-2025553 <afriely-2025553@kinal.edu.gt>
2026-07-26 03:26:53 -0600

    feat: se creo el model de animal

---
+++ Patitas-en-Busca-de-Un-Hogar/src/models/animal.ts
@@ -0,0 +1,19 @@
+export interface Animal {
+
+    id_animal: number;
+
+    nombreAnimal: string;
+
+    edadAnimal: number;
+
+    sexoAnimal: string;
+
+    estadoAnimal: string;
+
+    id_refugio: number;
+
+    id_especie: number;
+
+    id_raza: number;
+
+}
